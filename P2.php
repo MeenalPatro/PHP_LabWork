f6bab1c6df06b25421e28e4af4370756df8966fe
Meenal Patro <meenalpatro@gmail.com>
2026-06-07 18:15:49 +0530

    Add files via upload

---
+++ P2.php
@@ -0,0 +1,15 @@
+<?php
+$str = "Hello World";
+
+echo $str . " Meenal";
+print("<br>");
+print($str . " Meenal");
+
+
+$num = 123;
+echo "<br>";
+var_dump($num);
+
+echo "<br>";
+var_export($num, true);
+?>
