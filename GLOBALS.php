dcac4e96badbf3fbb845fed78ce945963c30bf9e
Meenal Patro <meenalpatro@gmail.com>
2026-06-10 09:52:10 +0530

    Add files via upload

---
+++ GLOBALS.php
@@ -0,0 +1,10 @@
+<?php  
+    $num1 = 5;     
+    $num2 = 13;      
+    function global_var()  
+    {  
+            $sum = $GLOBALS['num1'] + $GLOBALS['num2'];  
+            echo "Sum of global variables is: " .$sum;  
+    }  
+    global_var();  
+?> 
