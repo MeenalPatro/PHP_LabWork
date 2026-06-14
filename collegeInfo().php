7c05b92166569a7267941e3081b2432f8d14ec33
Meenal Patro <meenalpatro@gmail.com>
2026-06-14 19:44:42 +0530

    Add files via upload

---
+++ collegeInfo().php
@@ -0,0 +1,21 @@
+<?php
+
+/* 1. Create a function named collegeInfo() that displays:
+
+College Name
+
+Course Name 
+
+Academic Year */
+
+function collegeInfo()
+{
+    echo "<h3>College Information</h3>";
+    echo "College Name: Marwadi University<br>";
+    echo "Course Name: BCA<br>";
+    echo "Academic Year: 2024-2027<br><br>";
+}
+
+collegeInfo();
+
+?>
