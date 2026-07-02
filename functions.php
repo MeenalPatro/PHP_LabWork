0db0b29b571826540f301569cc7d04f2c73f05d3
Meenal Patro <meenalpatro@gmail.com>
2026-07-02 07:46:01 +0530

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+//Types of functions
+//1.Built-in functions
+//2.UDF
+//String functions are built-in functions.
+
+$str1="Meenal";
+$str2="Patro";
+
+echo $str1. "  " . $str2;
+echo "<br>";
+print("print" . " " . $str1 . " " . $str2);
